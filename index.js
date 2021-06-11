dfcddaabe0142cbc952d5fd88618f3531a684acd
Juan Mello <juanribeirofx@gmail.com>
2021-06-11 14:17:06 -0300

    ForEach

--- index.js
+++ index.js
@@ -7,3 +7,10 @@ var password = document.querySelector("#exampleInputPassword");
 var photo = document.querySelector("#exampleInputFile");
 var admin = document.querySelector("#exampleInputAdmin");
 
+var fields = document.querySelectorAll("#form-user-create [name]");
+
+fields.forEach(function(field, index) {
+
+    console.log(field.id, field.name, field.checked, index);
+
+});
