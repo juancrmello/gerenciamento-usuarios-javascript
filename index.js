993cb8b44583a046556bb9fdaa7a0e815f72e0eb
Juan Mello <juanribeirofx@gmail.com>
2021-06-13 18:06:37 -0300

    Trabalhando com JSON

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-var nome = document.querySelector("#exampleInputName");
-var gender = document.querySelector("#form-user-create [name=gender]:checked");
-var birth = document.querySelector("#exampleInputBirth");
-var country = document.querySelector("#exampleInputCountry");
-var email = document.querySelector("#exampleInputEmail");
-var password = document.querySelector("#exampleInputPassword");
-var photo = document.querySelector("#exampleInputFile");
-var admin = document.querySelector("#exampleInputAdmin");
-
 var fields = document.querySelectorAll("#form-user-create [name]");
+var user = {};
 
 fields.forEach(function(field, index) {
 
-    console.log(field.id, field.name, field.checked, index);
+    if(field.name == "gender") {
+        
+        if (field.checked) {
+            
+            user[field.name] = field.value;
 
+        }
+
+    } else {
+
+        user[field.name] = field.value;
+
+    }
 });
