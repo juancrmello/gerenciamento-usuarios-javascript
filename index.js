fb3cfc02d88f2c509ea5443b3ef92e4613b47038
Juan Mello <juanribeirofx@gmail.com>
2021-06-14 20:07:35 -0300

    Classes e MVC

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ var user = {};
 
 function addLine(dataUser) {
 
-    var tr = document.createElement("tr");
-
-    tr.innerHTML = `
+    document.getElementById("table-users").innerHTML = `
         <tr>
             <td>
                 <img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm">
@@ -19,7 +17,7 @@ function addLine(dataUser) {
                 <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
             </td>
         </tr>
-    `
+    `;
 
     document.getElementById("table-users").appendChild(tr);
 
@@ -46,6 +44,8 @@ document.getElementById("form-user-create").addEventListener("submit", function(
         }
     });
 
-    addLine(user);
+    var objectUser = new User(user.name, user.gender, user.birth, user.country, user.email, user.password, user.photo, user.admin);
+
+    addLine(objectUser);
 
 })
