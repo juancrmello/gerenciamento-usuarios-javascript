6f0a001f4e54f780bda859afbc01d611b28082bc
Juan Mello <juanribeirofx@gmail.com>
2021-06-16 14:04:32 -0300

    Validando formularios

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -89,7 +89,16 @@ class UserController {
 
         let user = {};
 
+        let isValid = true;
+
         [...this.formEl.elements].forEach(function(field, index) {
+
+            if (['name', 'email', 'password'].indexOf(field.name) > -1 && !field.value) {
+
+                field.parentElement.classList.add('has-error');
+                isValid = false;
+
+            }
     
             if(field.name == "gender") {
                 
@@ -109,6 +118,10 @@ class UserController {
         
             }
         });
+
+        if(!isValid) {
+            return false
+        }
 
         return new User(user.name, user.gender, user.birth, user.country, user.email, user.password, user.photo, user.admin);
 
