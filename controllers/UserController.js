8a66e1be0370d6e08ce906fd93010fe73afa6ebf
Juan Mello <juanribeirofx@gmail.com>
2021-06-16 15:15:10 -0300

    Usando dataset

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -20,6 +20,8 @@ class UserController {
 
             let values = this.getValues();
 
+            if (!values) return false;
+
             this.getPhoto().then((content) => {
 
                 values.photo = content;
@@ -131,6 +133,8 @@ class UserController {
 
         let tr = document.createElement('tr');
 
+        tr.dataset.user = JSON.stringify(dataUser);
+
         tr.innerHTML = `
             <td>
                 <img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm">
@@ -147,6 +151,31 @@ class UserController {
     
         this.tableEl.appendChild(tr);
 
+        this.updateCount();
+
     };
 
+    updateCount() {
+
+        let numberUsers = 0;
+        let numberAdmin = 0;
+
+        [...this.tableEl.children].forEach(tr=>{
+
+            numberUsers++;
+
+            let user = JSON.parse(tr.dataset.user);
+        
+            if (user._admin) numberAdmin++;
+
+            console.log(user);
+
+        });
+
+        
+
+        document.querySelector("#numbers-user").innerHTML = numberUsers;
+        document.querySelector("#numbers-user-admin").innerHTML = numberAdmin;
+
+    }
 }
